979d8aa82fdd16ddd2f503f2cee5cb542805beb2
Rishal <Rishal.mhd009@gmail.com>
2023-05-17 14:32:19 +0530

    user state to get datas

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+  };
+
+  const usernameChangehandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangehandler = (event) => {
+    setEnteredAge(event.target.value);
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangehandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangehandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
